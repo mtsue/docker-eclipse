b60eb5c3d369434739959b4e422c7869e5897caa
mtsue <bolerock309@gmail.com>
2017-04-03 01:52:22 +0900

    nameオプション追加

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 mkdir -p $(cd $(dirname $0) && pwd)/workspace
 
 sudo docker run -it \
+  --name docker-eclipse \
   --env DISPLAY \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
   --volume $(cd $(dirname $0) && pwd)/workspace:/home/eclipser/workspace \
