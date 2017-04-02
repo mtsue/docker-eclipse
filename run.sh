162e361eb487bdea9d78c268474674cf6460802b
mtsue <bolerock309@gmail.com>
2017-04-03 01:48:25 +0900

    rmオプションを削除

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 mkdir -p $(cd $(dirname $0) && pwd)/workspace
 
-sudo docker run -it --rm \
+sudo docker run -it \
   --env DISPLAY \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
   --volume $(cd $(dirname $0) && pwd)/workspace:/home/eclipser/workspace \
